7e2aea6e5a0e253f0ea2d4ad69b1585521034a47
23eerick <erickwelberth1@hotmail.com>
2022-09-05 11:27:10 -0300

    feat: added router

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/primary-router', (req, res) => {
+    return res.json({
+        message: 'Acessou a primeira rota'
+    })
+})
+
 app.listen(3001, () => {
     console.log('server running!')
 })
